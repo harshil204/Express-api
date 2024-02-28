a06028aebe35df7828ca3df9024389282ef31a68
Lucifer Morningstar <lucifer@morningstar.com>
2024-02-28 23:48:49 +0530

    cors added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const connectDB = require("./config/dbConnection")
 const errorHandler = require("./middleware/errorHandler");
+const cors = require("cors")
 const dotenv = require("dotenv").config();
 
 const app = express()
@@ -10,6 +11,7 @@ const port = process.env.PORT || 5000
 connectDB()
 
 app.use(express.json())
+app.use(cors())
 app.use("/api/user", require("./routes/userRoutes"))
 app.use("/api/contacts", require("./routes/contactRoutes"))
 app.use("/api/expenses", require("./routes/expenseRoutes"))
